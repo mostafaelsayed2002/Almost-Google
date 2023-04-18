bc248596d53fb97207cdad4eeb2f526400f5dd48
Mostafa Elsayed <mac@macs-MacBook-Air.local>
2023-04-18 14:47:44 +0200

    Starting Point

---
+++ src/main/java/AlmostGoogle/AlmostGooogleApplication.java
@@ -0,0 +1,13 @@
+package AlmostGoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmostGooogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmostGooogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlmostGoogle/AlmostGooogleApplicationTests.java
@@ -0,0 +1,13 @@
+package AlmostGoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmostGooogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
