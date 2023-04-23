53be2716f5dae08c193cc00c3dff05830865c653
Mostafa Elsayed <mac@macs-MacBook-Air.local>
2023-04-23 05:12:34 +0200

    Indexer Done

---
+++ backend/src/main/java/SearchEngine/Website.java
@@ -0,0 +1,17 @@
+package SearchEngine;
+
+
+public class Website  {
+
+    public String url;
+    public String[] places;
+    public int TF;
+
+
+    Website() {
+        places = new String[8];
+        url = "";
+        TF = 0;
+    }
+}
+
