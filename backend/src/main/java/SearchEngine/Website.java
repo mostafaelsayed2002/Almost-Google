8e9e95d49d32af75ffc21e052659510a587bdd56
Mostafa Elsayed <mac@macs-MacBook-Air.local>
2023-04-29 21:52:42 +0300

    indexer done

--- backend/src/main/java/SearchEngine/Website.java
+++ backend/src/main/java/SearchEngine/Website.java
@@ -1,16 +1,21 @@
 package SearchEngine;
 
 
+import kotlin.Pair;
+
+import java.util.Vector;
+
 public class Website  {
 
     public String url;
-    public String[] places;
+   // public String[] places;
+    public Vector<Pair<String,String>>places;
     public int TF;
+    public String title;
 
 
     Website() {
-        places = new String[8];
-        url = "";
+        places = new Vector<>();
         TF = 0;
     }
 }
